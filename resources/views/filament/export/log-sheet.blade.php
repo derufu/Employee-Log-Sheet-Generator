0cb6dab3ebbacaa4bb34d25e32cf776a5fb20518
Delf <41110569+derufu@users.noreply.github.com>
2025-03-04 14:17:37 +0800

    Favicon

--- resources/views/filament/export/log-sheet.blade.php
+++ resources/views/filament/export/log-sheet.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Log Sheet</title>
+    <link rel="icon" href="https://www.davaocity.gov.ph/wp-content/uploads/2020/06/cropped-davao-city-logo-32x32.png" sizes="32x32">
 </head>
 <style>
     body {
---
+++ resources/views/vendor/filament/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>{{ config('app.name') }}</title>
+    <link rel="icon" href="https://www.davaocity.gov.ph/wp-content/uploads/2020/06/cropped-davao-city-logo-32x32.png" sizes="32x32">
+</head>
