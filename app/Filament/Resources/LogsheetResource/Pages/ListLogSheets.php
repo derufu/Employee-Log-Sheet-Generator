4a6a5b02efcb9d89bdab418d9115e7026744238a
Delf <41110569+derufu@users.noreply.github.com>
2025-03-04 09:36:20 +0800

    Add select employee option on generating log sheet

--- app/Filament/Resources/LogsheetResource/Pages/ListLogSheets.php
+++ app/Filament/Resources/LogsheetResource/Pages/ListLogSheets.php
@@ -16,6 +16,7 @@ use Illuminate\Support\Facades\Storage;
 class ListLogSheets extends ListRecords
 {
     protected static string $resource = LogSheetResource::class;
+
     protected function getHolidays($year)
     {
         return [
@@ -50,6 +51,7 @@ class ListLogSheets extends ListRecords
         }
         return false;
     }
+
     protected function getHeaderActions(): array
     {
         return [
@@ -63,7 +65,39 @@ class ListLogSheets extends ListRecords
                             'contract_of_service' => 'Contract of Service',
                             'plantilla' => 'Plantilla',
                         ])
-                        ->required(),
+                        ->required()
+                        ->reactive()
+                        ->afterStateUpdated(fn (callable $set) => $set('employee_ids', [])),
+                    Forms\Components\Select::make('employee_ids')
+                        ->label('Employees')
+                        ->multiple()
+                        ->options(function (callable $get) {
+                            $position_type = $get('position_type');
+                            return Employee::where('position_type', $position_type)
+                                ->get()
+                                ->mapWithKeys(function ($employee) {
+                                    $middleInitial = $employee->middle_name ? substr($employee->middle_name, 0, 1) . '.' : '';
+                                    return [$employee->id => $employee->first_name . ' ' . $middleInitial . ' ' . $employee->last_name];
+                                });
+                        })
+                        ->required()
+                        ->reactive()
+                        ->afterStateUpdated(function (callable $set, $state) {
+                            if (in_array('all', $state)) {
+                                $set('employee_ids', Employee::pluck('id')->toArray());
+                            }
+                        })
+                        ->placeholder('Select employees or choose "All"')
+                        ->options(function (callable $get) {
+                            $position_type = $get('position_type');
+                            $employees = Employee::where('position_type', $position_type)
+                                ->get()
+                                ->mapWithKeys(function ($employee) {
+                                    $middleInitial = $employee->middle_name ? substr($employee->middle_name, 0, 1) . '.' : '';
+                                    return [$employee->id => $employee->first_name . ' ' . $middleInitial . ' ' . $employee->last_name];
+                                });
+                            return ['all' => 'All'] + $employees->toArray();
+                        }),
                     Forms\Components\DatePicker::make('month')
                         ->label('Select Month')
                         ->required()
@@ -71,69 +105,22 @@ class ListLogSheets extends ListRecords
                         ->displayFormat('F Y')
                         ->default(now()->format('Y-m')),
                 ])
-                // ->action(function (array $data) {
-                //     $month = Carbon::parse($data['month'])->format('m');
-                //     $year = Carbon::parse($data['month'])->format('Y');
-                //     $current_time = date('Y-m-d h:i:s A');
-                //     $position_type = $data['position_type'];
-                //     $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
-                //     $weekdays = [];
-                //     $employees = Employee::where('position_type', $position_type)
-                //         ->with(['leaves' => function ($query) use ($year, $month) {
-                //             $query->whereMonth('start_date', $month)
-                //                 ->whereYear('end_date', $year)
-                //                 ->where('status', 'approved');
-                //         }])
-                //         ->with(['workFromHomes' => function ($query) use ($year, $month) {
-                //             $query->whereMonth('start_date', $month)
-                //                 ->whereYear('end_date', $year);
-                //         }])
-                //         ->orderBy('last_name', 'asc')
-                //         ->get();
-
-                //     for ($day = 1; $day <= $numDays; $day++) {
-                //         $dayOfWeek = date('N', strtotime("$year-$month-$day"));
-                //         if ($dayOfWeek >= 1 && $dayOfWeek <= 5) {
-                //             $formattedDate = date('Y-m-d', strtotime("$year-$month-$day"));
-                //             $weekdays[] = $formattedDate;
-                //         }
-                //     }
-
-                //     $options = new Options();
-                //     $options->set('isHtml5ParserEnabled', true);
-                //     $options->set('isPhpEnabled', true);
-                //     $dompdf = new Dompdf($options);
-
-                //     $html = view('filament.export.log-sheet', compact('employees', 'weekdays'))->render();
-                //     $dompdf->loadHtml($html);
-                //     $dompdf->setPaper('letter', 'portrait');
-                //     $dompdf->render();
-
-                //     $output = $dompdf->output();
-                //     $filePath = "logsheets/{$year}/{$month}/log_sheet_{$position_type}_{$current_time}.pdf";
-                //     Storage::disk('public')->makeDirectory("logsheets/{$year}/{$month}");
-                //     Storage::disk('public')->put($filePath, $output);
-                //     LogSheet::create([
-                //         'filename' => basename($filePath), // Extracts just the filename
-                //         'filepath' => $filePath,
-                //         'year' => $year,
-                //         'month' => $month,
-                //     ]);
-
-
-                //     return response()->download(storage_path("app/public/{$filePath}"));
-                // })
-                // ->icon('heroicon-o-document-text'),
-
                 ->action(function (array $data) {
                     $month = Carbon::parse($data['month'])->format('m');
                     $year = Carbon::parse($data['month'])->format('Y');
                     $current_time = date('Y-m-d_H-i-s_A'); // Fix timestamp format
                     $position_type = $data['position_type'];
+                    $employee_ids = $data['employee_ids'];
+
+                    if (in_array('all', $employee_ids)) {
+                        $employee_ids = Employee::where('position_type', $position_type)->pluck('id')->toArray();
+                    }
+
                     $numDays = cal_days_in_month(CAL_GREGORIAN, $month, $year);
                     $weekdays = [];
 
-                    $employees = Employee::where('position_type', $position_type)
+                    $employees = Employee::whereIn('id', $employee_ids)
+                        ->where('position_type', $position_type)
                         ->with(['leaves' => function ($query) use ($year, $month) {
                             $query->whereMonth('start_date', $month)
                                 ->whereYear('end_date', $year)
@@ -181,7 +168,6 @@ class ListLogSheets extends ListRecords
                     return response()->download(Storage::disk('public')->path($filePath));
                 })
                 ->icon('heroicon-o-document-text'),
-
         ];
     }
 }
